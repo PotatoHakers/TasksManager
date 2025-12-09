30b153ce1b906d56b14a24f4894fd3d52f00d00b
PotatoHakers <s.herza@mail.ru>
2025-12-09 16:05:39 +0300

    Add validate anti forgery token

--- TaskManager/Controllers/AccountController.cs
+++ TaskManager/Controllers/AccountController.cs
@@ -52,6 +52,7 @@ public class AccountController : Controller
     }
 
     [HttpPost]
+    [ValidateAntiForgeryToken]
     public async Task<IActionResult> Logout()
     {
         await _signInManager.SignOutAsync();
